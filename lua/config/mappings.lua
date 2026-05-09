f6c09f7b2313aad4ce9af1a2a923431f251f41e8
Xenex <timo.wolf@gmx.ch>
2026-05-09 09:27:07 +0200

    tmux navigator

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,3 +1,11 @@
 local map = vim.keymap.set
 
+-- simple save
 map("n", "<leader>w", "<CMD>w<CR>", { desc = "save the current file" })
+
+-- easy escape
+vim.keymap.set("i", "jj", "<esc>")
+vim.keymap.set("i", "jk", "<esc>")
+
+-- noh
+vim.keymap.set("n", "<esc>", "<cmd>noh<CR>")
--- lua/config/setings.lua
+++ lua/config/setings.lua
@@ -1,33 +1,42 @@
 -- base settings
-vim.opt.number = true
+vim.opt.number = false
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 
 -- helper function
 local function set_relative()
-	vim.opt.relativenumber = true
+    vim.opt.relativenumber = true
+    vim.opt.number = false
 end
 
 local function set_absolute()
-	vim.opt.relativenumber = false
+    vim.opt.relativenumber = false
+    vim.opt.number = true
 end
 
 -- autocmds
 vim.api.nvim_create_autocmd("InsertEnter", {
-	callback = set_absolute,
+    callback = set_absolute,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-	callback = set_relative,
+    callback = set_relative,
 })
 
 vim.api.nvim_create_autocmd("FocusLost", {
-	callback = set_absolute,
+    callback = set_absolute,
 })
 
 vim.api.nvim_create_autocmd("FocusGained", {
-	callback = set_relative,
+    callback = set_relative,
 })
 
 -- Use system clipboard for all yank/delete/paste
 vim.opt.clipboard = "unnamedplus"
+
+-- tabstop
+vim.opt.expandtab = true   -- use spaces instead of tabs
+vim.opt.shiftwidth = 4     -- size of an indent
+vim.opt.tabstop = 4        -- how many spaces a tab counts for
+vim.opt.softtabstop = 4    -- backspace + editing behavior
+vim.opt.smartindent = true -- auto-indent new lines
