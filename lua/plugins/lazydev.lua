86b027bd5e1e174f83b8205324aaa77719b0850f
Xenex <timo.wolf@gmx.ch>
2026-06-13 12:38:09 +0200

    fix: disable lazydev lspconfig integration to prevent root_dir conflict

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,9 +1,12 @@
 return {
   "folke/lazydev.nvim",
-  event = "VeryLazy",
   opts = {
     library = {
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    },
+    integrations = {
+      lspconfig = false,
+      cmp = true,
     },
   },
 }
