cdd4f31805a57a8394961f0cb5eb052da10e9c07
Xenex <timo.wolf@gmx.ch>
2026-06-13 11:58:21 +0200

    feat: add hdg, inc, incq, main, for, ifdef to C++ snippets for parity with C

--- lua/snippets/cpp.lua
+++ lua/snippets/cpp.lua
@@ -5,7 +5,69 @@ local i = ls.insert_node
 local f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
 
+local function guard_name()
+  local name = vim.fn.expand("%:t:r")
+  return name:upper():gsub("[^%w]", "_") .. "_H"
+end
+
 return {
+  s("hdg", fmt([[
+#ifndef {}
+#define {}
+
+{}
+
+#endif /* {} */
+]], {
+    f(guard_name),
+    f(guard_name),
+    i(0),
+    f(guard_name),
+  })),
+
+  s("inc", fmt([[
+#include <{}>
+]], { i(1) })),
+
+  s("incq", fmt([[
+#include "{}"
+]], { i(1) })),
+
+  s("main", fmt([[
+int main(int argc, char *argv[])
+{{
+  {}
+
+  return 0;
+}}
+]], { i(0) })),
+
+  s("for", fmt([[
+for (int {} = {}; {} < {}; {}++) {{
+  {}
+}}
+]], {
+    i(1, "i"), i(2, "0"), i(1), i(3, "n"), i(1), i(0),
+  })),
+
+  s("struct", fmt([[
+struct {} {{
+  {}
+}};
+]], { i(1, "Name"), i(0) })),
+
+  s("enum", fmt([[
+enum class {} {{
+  {}
+}};
+]], { i(1, "Name"), i(0) })),
+
+  s("ifdef", fmt([[
+#ifdef {}
+{}
+#endif
+]], { i(1), i(0) })),
+
   s("cout", fmt([[
 std::cout << {} << std::endl;
 ]], { i(1) })),
@@ -35,18 +97,6 @@ std::vector<{}> {};
   s("auto", fmt([[
 auto {} = {};
 ]], { i(1, "name"), i(2, "expr") })),
-
-  s("struct", fmt([[
-struct {} {{
-  {}
-}};
-]], { i(1, "Name"), i(0) })),
-
-  s("enum", fmt([[
-enum class {} {{
-  {}
-}};
-]], { i(1, "Name"), i(0) })),
 
   s("range-for", fmt([[
 for (const auto& {} : {}) {{
