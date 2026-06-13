86b345935fec6bea48b9336c57bb3fdb16db8b31
Xenex <timo.wolf@gmx.ch>
2026-06-13 12:15:43 +0200

    style: nvchad-style bufferline with slant separators, numbers, hover, <leader>N navigation

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,12 +5,15 @@ return {
   opts = {
     options = {
       mode = "buffers",
-      separator_style = "thin",
+      separator_style = "slant",
       always_show_bufferline = true,
       show_buffer_close_icons = true,
       show_close_icon = false,
       color_icons = true,
       diagnostics = "nvim_lsp",
+      numbers = function(opts)
+        return string.format("%s", opts.ordinal)
+      end,
       offsets = {
         {
           filetype = "neo-tree",
@@ -19,11 +22,23 @@ return {
           separator = true,
         },
       },
+      hover = {
+        enabled = true,
+        delay = 100,
+        reveal = { "close" },
+      },
     },
   },
-  keys = {
-    { "[b", "<cmd>BufferLineCyclePrev<CR>", desc = "Previous buffer" },
-    { "]b", "<cmd>BufferLineCycleNext<CR>", desc = "Next buffer" },
-    { "<leader>bd", "<cmd>bd<CR>", desc = "Close buffer" },
-  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+    local map = vim.keymap.set
+    map("n", "[b", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+    map("n", "]b", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+    map("n", "<leader>bd", "<cmd>bd<CR>", { desc = "Close buffer" })
+    for i = 1, 9 do
+      map("n", "<leader>" .. i, function()
+        require("bufferline").go_to(i, true)
+      end, { desc = "Go to buffer " .. i })
+    end
+  end,
 }
