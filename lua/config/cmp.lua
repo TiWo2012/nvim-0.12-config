c56b7f5487be53ae40f5f3bf8d2703eb33340062
Xenex <timo.wolf@gmx.ch>
2026-06-24 19:30:33 +0200

    removed useless toy languages and added clangd

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -4,54 +4,54 @@ local ls = require("luasnip")
 require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/lua/snippets" })
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            ls.lsp_expand(args.body)
-        end,
-    },
+  snippet = {
+    expand = function(args)
+      ls.lsp_expand(args.body)
+    end,
+  },
 
-    mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif ls.expand_or_jumpable() then
-                ls.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif ls.expand_or_jumpable() then
+        ls.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
 
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif ls.jumpable(-1) then
-                ls.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif ls.jumpable(-1) then
+        ls.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
 
-    sources = {
-        { name = "nvim_lsp" },
-        { name = "path" },
-        { name = "luasnip" },
-    },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "luasnip" },
+  },
 
+  completion = {
+    autocomplete = { "InsertEnter", "TextChanged" },
+  },
+
+  window = {
     completion = {
-        autocomplete = { "InsertEnter", "TextChanged" },
+      border = "rounded",
+      winhighlight = "NormalFloat:Pmenu,CursorLine:PmenuSel",
     },
-
-    window = {
-        completion = {
-            border = "rounded",
-            winhighlight = "NormalFloat:Pmenu,CursorLine:PmenuSel",
-        },
-        documentation = {
-            border = "rounded",
-        },
+    documentation = {
+      border = "rounded",
     },
+  },
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,8 +8,7 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
-        "pyright",
-        "ts_ls",
+        "clangd",
       },
       automatic_enable = true,
     },
